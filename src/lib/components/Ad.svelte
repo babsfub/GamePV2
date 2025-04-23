0d22101aa38a64260f6a50ff440e4cda90dbb785
baboon <92304020+babsfub@users.noreply.github.com>
2025-04-23 13:42:37 +0200

    Update Ad.svelte

--- src/lib/components/Ad.svelte
+++ src/lib/components/Ad.svelte
@@ -8,14 +8,13 @@
   let activeSrc: string | null = null;
 
   function updateBanner() {
-    // Adaptez pageId et sizes comme dans votre ancien code…
     const id = '2391419';
     activeSrc = `//acceptable.a-ads.com/${id}`;
   }
 
   onMount(() => {
     if (!browser || !enableAds) return;
-    updateBanner();                     // insertion immédiate
+    updateBanner();                     
     window.addEventListener('resize', updateBanner);
     return () => window.removeEventListener('resize', updateBanner);
   });
@@ -28,7 +27,7 @@
       data-aa="2391419"
       title="Advertisement"
       class="ad-iframe"
-      width="728" height="90"
+      width="100%" height="100%"
       
     ></iframe>
     <noscript>
@@ -36,9 +35,7 @@
         <img src="//acceptable.a-ads.com/2391419.png" alt="Advertise here" style="width:100%;height:auto;" />
       </a>
     </noscript>
-    <a class="advertise-link" href="https://aads.com/campaigns/new/?source_id=2391419">
-      Advertise here
-    </a>
+   
   </div>
 {/if}
 
@@ -48,12 +45,12 @@
     overflow: visible !important;
   }
   .ad-container {
-    position: relative;
-    z-index: 20;
-    width: 728px;
-    max-width: 100%;
-    height: 90px;
-  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
   .ad-iframe {
     width: 100%;
     height: 100%;
@@ -61,13 +58,5 @@
     position: relative;
     z-index: 10;
   }
-  .advertise-link {
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    font-size: 11px;
-    background: rgba(0,0,0,0.5);
-    padding: 2px 5px;
-    z-index: 30;
-  }
+  
 </style>
